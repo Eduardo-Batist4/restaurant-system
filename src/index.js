e8f331f07f271ec2ad85b55069a50c9368571049
Eduardo-Batist4 <carloswe3st@gmail.com>
2024-07-26 11:11:22 -0300

    importando router e express.json

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const sequelize = require('../config/sequelize');
+const router = require('../routes/routes');
 require('dotenv').config();
 
 const port = process.env.PORT;
 const app = express();
+
+app.use(express.json());
+app.use(router);
 
 sequelize.sync().then(() => {
     app.listen(port);
