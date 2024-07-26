baa0406a7aed305b858ac8520c85077ce9321920
Eduardo-Batist4 <carloswe3st@gmail.com>
2024-07-26 20:11:23 -0300

    rotas para table criadas

--- routes/routes.js
+++ routes/routes.js
@@ -3,12 +3,21 @@ const router = express.Router();
 
 // Controllers
 const CustomerController = require('../controllers/CustomerController');
+const TableController = require('../controllers/TableController');
 
-
+// Customers
 router.post('/customers', CustomerController.createCustomer);
 router.get('/customers', CustomerController.getCustomers);
 router.get('/customers/:id', CustomerController.getCustomer);
 router.patch('/customers/:id', CustomerController.updateCustomer);
 router.delete('/customers/:id', CustomerController.deleteCustomer);
 
-module.exports = router;
+// Tables
+router.post('/tables', TableController.createTable);
+router.get('/tables', TableController.getTables);
+router.get('/tables/:id', TableController.getTable);
+router.patch('/tables/:id', TableController.updateTable);
+router.delete('/tables/:id', TableController.deleteTable);
+
+
+module.exports = router; 
