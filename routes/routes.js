a1bbce1924e50ba2b01593ef048f7fcb45ab3669
Eduardo-Batist4 <carloswe3st@gmail.com>
2024-07-29 11:53:49 -0300

    rotas para order

--- routes/routes.js
+++ routes/routes.js
@@ -5,26 +5,33 @@ const router = express.Router();
 const CustomerController = require('../controllers/CustomerController');
 const TableController = require('../controllers/TableController');
 const MenuController = require('../controllers/MenuController');
+const OrderController = require('../controllers/OrderController');
 
 // Customers
 router.post('/customers', CustomerController.createCustomer);
 router.get('/customers', CustomerController.getCustomers);
-router.get('/customers/:id', CustomerController.getCustomer);
-router.patch('/customers/:id', CustomerController.updateCustomer);
-router.delete('/customers/:id', CustomerController.deleteCustomer);
+router.get('/customer/:id', CustomerController.getCustomer);
+router.patch('/customer/:id', CustomerController.updateCustomer);
+router.delete('/customer/:id', CustomerController.deleteCustomer);
 
 // Tables
 router.post('/tables', TableController.createTable);
 router.get('/tables', TableController.getTables);
-router.get('/tables/:id', TableController.getTable);
-router.patch('/tables/:id', TableController.updateTable);
-router.delete('/tables/:id', TableController.deleteTable);
+router.get('/table/:id', TableController.getTable);
+router.patch('/table/:id', TableController.updateTable);
+router.delete('/table/:id', TableController.deleteTable);
 
 // Menu
-router.post('/menus', MenuController.createMenus);
+router.post('/menus', MenuController.createMenu);
 router.get('/menus', MenuController.getMenus);
 router.patch('/menu/:id', MenuController.updateMenu);
 router.delete('/menu/:id', MenuController.deleteMenu);
 
+// Orders
+router.post('/orders', OrderController.createOrder);
+router.get('/orders', OrderController.getOrders);
+router.get('/order/:id', OrderController.getOrder);
+router.patch('/order/:id', OrderController.updateOrder);
+router.delete('/order/:id', OrderController.deleteOrder);
 
 module.exports = router; 
