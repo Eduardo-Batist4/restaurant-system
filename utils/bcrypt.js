4c8f39a5decd2d452d95e35a6550ba02843ec794
Eduardo-Batist4 <carloswe3st@gmail.com>
2024-07-30 13:20:42 -0300

    utils para adição de hash e verificação de senha

---
+++ utils/bcrypt.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcrypt');
+
+async function hashPassword(password) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+        return hash;
+    } catch (error) {
+        console.log('Error generating hash.');
+        throw error;
+    }
+};
+
+async function verifyPassword(password, hash) {
+    try {
+        const match = await bcrypt.compare(password, hash);
+        return match;
+    } catch (error) {
+        console.log('Password verification error.');
+        throw error;
+    }
+};
+
+module.exports = { hashPassword, verifyPassword }
