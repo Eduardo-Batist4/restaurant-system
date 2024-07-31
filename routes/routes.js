46a24b299f1285c1208e3aca8cbdeac48f11e7f9
Eduardo-Batist4 <carloswe3st@gmail.com>
2024-07-31 16:57:11 -0300

    rotas para função login e logout

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,7 @@ const TableController = require('../controllers/TableController');
 const MenuController = require('../controllers/MenuController');
 const OrderController = require('../controllers/OrderController');
 const EmployeeController = require('../controllers/EmployeeController');
+const LoginController = require('../controllers/LoginController');
 
 // Customers
 router.post('/customers', CustomerController.createCustomer);
@@ -23,23 +24,26 @@ router.patch('/table/:id', TableController.updateTable);
 router.delete('/table/:id', TableController.deleteTable);
 
 // Menu
-router.post('/menus', MenuController.createMenu);
+router.post('/menus', LoginController.verifyToken, MenuController.createMenu);
 router.get('/menus', MenuController.getMenus);
-router.patch('/menu/:id', MenuController.updateMenu);
-router.delete('/menu/:id', MenuController.deleteMenu);
+router.patch('/menu/:id', LoginController.verifyToken, MenuController.updateMenu);
+router.delete('/menu/:id', LoginController.verifyToken, MenuController.deleteMenu);
 
 // Orders
-router.post('/orders', OrderController.createOrder);
-router.get('/orders', OrderController.getOrders);
-router.get('/order/:id', OrderController.getOrder);
-router.patch('/order/:id', OrderController.updateOrder);
-router.delete('/order/:id', OrderController.deleteOrder);
+router.post('/orders', LoginController.verifyToken, OrderController.createOrder);
+router.get('/orders', LoginController.verifyToken, OrderController.getOrders);
+router.get('/order/:id', LoginController.verifyToken, OrderController.getOrder);
+router.patch('/order/:id', LoginController.verifyToken, OrderController.updateOrder);
+router.delete('/order/:id', LoginController.verifyToken, OrderController.deleteOrder);
 
 // Employees
 router.post('/employees', EmployeeController.createEmployee);
-router.get('/employees', EmployeeController.getEmployees);
-router.get('/employee/:id', EmployeeController.getEmployee);
-router.delete('/employee/:id', EmployeeController.deleteEmployee);
+router.get('/employees', LoginController.verifyToken, EmployeeController.getEmployees);
+router.get('/employee/:id', LoginController.verifyToken, EmployeeController.getEmployee);
+router.delete('/employee/:id', LoginController.verifyToken, EmployeeController.deleteEmployee);
 
+// Login
+router.post('/login', LoginController.login);
+router.post('/logout', LoginController.logout);
 
 module.exports = router; 
