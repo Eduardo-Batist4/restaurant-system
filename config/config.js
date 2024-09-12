b51d0507d7e2f2567a7dc1c075f801e9a6490b43
Eduardo-Batist4 <carloswe3st@gmail.com>
2024-09-12 15:46:12 -0300

    alterando config do db, adicionando pasta/arquivo para teste com jest

--- config/config.js
+++ config/config.js
@@ -4,15 +4,8 @@ module.exports = {
     development: {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE_DEVELOPMENT,
+        database: process.env.DB_DATABASE,
         host: process.env.DB_HOST,
         dialect: 'postgres'
-    },
-    test: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE_TEST,
-        host: process.env.DB_HOST,
-        dialect: 'postgres'
-    },
+    }
 }
