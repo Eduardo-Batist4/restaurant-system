92fd9c8b330379b5249d064690e48a561dda7981
Eduardo-Batist4 <carloswe3st@gmail.com>
2024-09-27 18:22:32 -0300

    todos os teste suites para o Employee feito

---
+++ __tests__/Controllers/Employee/Employee.delete.test.js
@@ -0,0 +1,54 @@
+const { deleteEmployee } = require('../../../controllers/EmployeeController');
+const Employee = require('../../../models/Employee');
+
+jest.mock('../../../models/Employee');
+
+describe('Delete Employee', () => {
+    let req, res;
+
+    beforeEach(() => {
+        req = { 
+            params: { id: '1' },
+            body: {}, 
+        },
+        res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('Should return (200) if the Employee is deleted', async () => {
+        Employee.findByPk.mockResolvedValue(true);
+        Employee.destroy.mockResolvedValue(true)
+
+        await deleteEmployee(req, res);
+
+        expect(Employee.findByPk).toHaveBeenCalledWith(req.params.id);
+        expect(Employee.destroy).toHaveBeenCalledWith({ where: { id: '1'} });
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith('Success!');
+    });
+
+    it('Should return (404) if the Employee is not found', async () => {
+        Employee.findByPk.mockResolvedValue(false);
+
+        await deleteEmployee(req, res);
+
+        expect(Employee.findByPk).toHaveBeenCalledWith('1');
+        expect(res.status).toHaveBeenCalledWith(404);
+        expect(res.json).toHaveBeenCalledWith({ error: 'Not Found!'});
+    });
+
+    it('Should handle internal server erros', async () => {
+        Employee.findByPk.mockRejectedValue(new Error('Database Error'));
+
+        await deleteEmployee(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.json).toHaveBeenCalledWith({ error: 'Internal Server Error.' });
+    });
+});
