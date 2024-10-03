363580facfe78af8d4213d3ecc0f490d34d8d9c5
Eduardo-Batist4 <carloswe3st@gmail.com>
2024-10-03 14:13:30 -0300

    todos os teste suites para getMenus feito

---
+++ __tests__/Controllers/Menu/Menu.read.test.js
@@ -0,0 +1,53 @@
+const { getMenus } = require('../../../controllers/MenuController');
+const { Menu } = require('../../../models/Association');
+
+jest.mock('../../../models/Association');
+
+describe('Create Menu', () => {
+    let req, res;
+
+    beforeEach(() => {
+        req = { body: {} };
+        res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('Should return (200) and all menu items', async () => {
+        const menuItems = [
+            {
+                name: 'Pizza',
+                description: 'Tomato, Cheese',
+                price: 20,
+                available: true
+            },
+            {
+                name: 'Hamburger',
+                description: 'Bread, Lettuce, Tomato, Cheese',
+                price: 20,
+                available: true
+            },
+        ]
+
+        Menu.findAll.mockResolvedValue(menuItems);
+
+        await getMenus(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith(menuItems);
+    });
+
+    it('Handle internal server erros', async () => {
+        Menu.findAll.mockRejectedValue(new Error('Database Error'));
+
+        await getMenus(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.json).toHaveBeenCalledWith({ error: 'Internal Server Error.'});
+    });
+});
